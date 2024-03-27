463e3898078df771f87ab39c1a83a1883e3e0462
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-26 17:56:42 -0700

    Completed task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,8 +6,5 @@ from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
-  """Lists all documents in a collection"""
-  for doc in mongo_collection:
-    mongo_collection.find()
-    
-  return doc
+    """Lists all documents in a collection"""
+    return [doc for doc in mongo_collection.find()]
