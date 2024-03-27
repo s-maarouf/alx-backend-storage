8943f73fef58c29f2e33757588bf143e997a48b1
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-27 01:41:45 +0000

    Completed task 14

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""MongoDB module"""
+
+from pymongo import MongoClient
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by average score"""
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
