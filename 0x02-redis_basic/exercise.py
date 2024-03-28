ba1d98b35f6926bbc603f85cac3dc99dc4b17828
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-28 00:31:30 +0000

    Implemented task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,22 +12,39 @@ def count_calls(method: Callable) -> Callable:
     """Counts how many times Cache was called"""
     @wraps(method)
     def invoker(self, *args, **kwargs) -> Any:
-        '''Invokes the given method after incrementing its call counter.
-        '''
+        """Invokes the given method after incrementing its call counter"""
         if isinstance(self._redis, redis.Redis):
             self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
     return invoker
 
 
+def call_history(method: Callable) -> Callable:
+    """Stores the history of inputs/outputs"""
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        """Returns the method's output after storing its inputs and output"""
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(out_key, output)
+        return output
+    return invoker
+
+
 class Cache:
     """Cache class"""
+
     def __init__(self) -> None:
         """stores an instance of redis"""
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, int, bytes, float]) -> str:
         """returns generated key"""
         data_key = str(uuid.uuid1())
